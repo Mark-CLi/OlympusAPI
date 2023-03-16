ba209a3126e4aea1c5690c169920e3672ed1b3d3
changzhi li <m5409362@gmail.com>
2023-03-16 11:23:20 -0700

    Working v0.2 with seper

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <curl/curl.h>
 
 int main(void) {
@@ -6,67 +7,87 @@ int main(void) {
     CURLcode res;
     char token[100]; //token
     char ID[100]; //player id
+    char input[100]; //user input
+    printf("Welcome to some ghetto ass API Poling Tool\n"); //welcome message
+    printf("MCL 2022 All right reserved, olympus-entertainment.com/profile/47738-mark_/\n"); //welcome message
 
+    while (1) {
+        printf("Enter feature (Server/Shed-ledger/Cartels/Players) or type 'stop' to exit: Caps Sensitives \n"); //ask user for feature input
+        scanf("%s", input);
 
-    printf("Enter token: "); //ask user for token input
-    scanf("%s", token);
+        if (strcmp(input, "stop") == 0) {
+            break;
+        }
 
-    printf("Enter Player ID: "); //ask user for player id input
-    scanf("%s", ID);
+        printf("Enter token(Visit Your Gang Profile on Oly Stats,DO NOT INCLUDE token):\n "); //ask user for token input
+        scanf("%s", token);
 
-    curl = curl_easy_init(); //initialize curl
-    if(curl) { //if curl is initialized
-        char url[200];
-        sprintf(url, "https://stats.olympus-entertainment.com/api/v3.0/players/?player_ids=%s", ID); //add player   id to url
+        printf("Enter Player ID/Shed ID\n"); //ask user for player id input
+        scanf("%s", ID);
 
-        struct curl_slist *headers = NULL;
-        char auth_header[100];
-        sprintf(auth_header, "Authorization: Token %s", token);
-        headers = curl_slist_append(headers, auth_header);
-        headers = curl_slist_append(headers, "accept: application/json");
+        curl = curl_easy_init(); //initialize curl
+        if(curl) { //if curl is initialized
+            char url[200];
+            if (strcmp(input, "Server") == 0) {
+                sprintf(url, "https://stats.olympus-entertainment.com/api/v3.0/servers/");
+            } else if (strcmp(input, "Shed-ledger") == 0) {
+                sprintf(url, "https://stats.olympus-entertainment.com/api/v3.0/shed_ledger/?player_ids=%s", ID);
+            } else if (strcmp(input, "Cartels") == 0) {
+                sprintf(url, "https://stats.olympus-entertainment.com/api/v3.0/cartels/");
+            } else if (strcmp(input, "Players") == 0) {
+                sprintf(url, "https://stats.olympus-entertainment.com/api/v3.0/players/?player_ids=%s", ID);
+            } else {
+                printf("Invalid input\n");
+                continue;
+            }
 
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+            struct curl_slist *headers = NULL;
+            char auth_header[100];
+            sprintf(auth_header, "Authorization: Token %s", token);
+            headers = curl_slist_append(headers, auth_header);
+            headers = curl_slist_append(headers, "accept: application/json");
 
-        res = curl_easy_perform(curl);
+            curl_easy_setopt(curl, CURLOPT_URL, url);
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-        if(res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        } else {
-            char *output_str;
-            long http_code = 0;
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-            if (http_code == 200) {
-                curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &output_str);
-                if (strstr(output_str, "application/json")) {
-                    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
-                    curl_easy_setopt(curl, CURLOPT_WRITEDATA, stdout);
-                    curl_easy_setopt(curl, CURLOPT_HEADER, 0);
+            res = curl_easy_perform(curl);
 
-                    size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
-                        size_t num_bytes = size * nmemb;
-                        char *comma_pos = strchr(ptr, ',');
-                        while (comma_pos != NULL) {
-                            printf("%.*s\n", (int)(comma_pos - ptr + 1), ptr);
-                            ptr = comma_pos + 1;
-                            comma_pos = strchr(ptr, ',');
+            if(res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            } else {
+                char *output_str;
+                long http_code = 0;
+                curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+                if (http_code == 200) {
+                    curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &output_str);
+                    if (strstr(output_str, "application/json")) {
+                        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
+                        curl_easy_setopt(curl, CURLOPT_WRITEDATA, stdout);
+                        curl_easy_setopt(curl, CURLOPT_HEADER, 0);
+
+                        size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+                            size_t num_bytes = size * nmemb;
+                            char *comma_pos = strchr(ptr, ',');
+                            while (comma_pos != NULL) {
+                                printf("%.*s\n", (int)(comma_pos - ptr + 1), ptr);
+                                ptr = comma_pos + 1;
+                                comma_pos = strchr(ptr, ',');
+                            }
+                            if (*ptr != '\0') {
+                                printf("%s", ptr);
+                            }
+                            return num_bytes;
                         }
-                        if (*ptr != '\0') {
-                            printf("%s", ptr);
-                        }
-                        return num_bytes;
+                        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+                        res = curl_easy_perform(curl);
                     }
-                    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-                    res = curl_easy_perform(curl);
                 }
             }
+            curl_slist_free_all(headers);
+            curl_easy_cleanup(curl);
         }
-        curl_slist_free_all(headers);
-        curl_easy_cleanup(curl);
+        printf("\n");
     }
 
-    void pause() {
-        printf("Press any key to continue...");
-        fgetc(stdin);
-    }
+    return 0;
 }
